bbe0d40d6d6ed8f0ca5e968991e530591d307e85
KimYoonMin <dbsalszz@naver.com>
2026-04-14 21:10:15 +0900

    Initial commit

---
+++ src/test/java/codeshop/codeshop/CodeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package codeshop.codeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
