8cdf876d474cd18d64efba0ef34184da83ddad29
KimYoonMin <dbsalszz@naver.com>
2026-04-15 21:33:55 +0900

    [#3] MemberServiceImpl PasswordEncoder 의존성 추가

--- src/main/java/codeshop/codeshop/application/MemberServiceImpl.java
+++ src/main/java/codeshop/codeshop/application/MemberServiceImpl.java
@@ -3,19 +3,24 @@ package codeshop.codeshop.application;
 import codeshop.codeshop.domain.entity.Member;
 import codeshop.codeshop.infra.MemberRepository;
 import codeshop.codeshop.presentation.dto.SignUpRequestDto;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public MemberServiceImpl(MemberRepository memberRepository) {
+    public MemberServiceImpl(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public Member signUp(SignUpRequestDto signUpRequestDto) {
-        Member member = Member.from(signUpRequestDto);
+        String email = signUpRequestDto.getEmail();
+        String password = signUpRequestDto.getPassword();
+        Member member = Member.createMemberWhenSignUp(email, passwordEncoder.encode(password));
         return memberRepository.save(member);
     }
 }
