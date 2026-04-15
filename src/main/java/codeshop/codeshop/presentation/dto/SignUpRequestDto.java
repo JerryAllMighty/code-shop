fad12f45bd22c49aaca0c9b75fbe188d50d7a26a
KimYoonMin <dbsalszz@naver.com>
2026-04-15 12:03:57 +0900

    [#1] 회원 가입 요청 dto 추가

---
+++ src/main/java/codeshop/codeshop/presentation/dto/SignUpRequestDto.java
@@ -0,0 +1,27 @@
+package codeshop.codeshop.presentation.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class SignUpRequestDto {
+
+    @NotNull(message = "이메일은 필수 입력값입니다")
+    @Email(message = "잘못된 이메일 형식입니다")
+    @Size(max = 255, message = "이메일 길이는 255자 이하여야 합니다")
+    private String email;
+
+    @NotNull(message = "비밀번호는 필수 입력값입니다")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 ~ 20자까지 허용됩니다")
+    private String password;
+
+    protected SignUpRequestDto(){}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
