9ab7518778f18ce87c6e46114a35e5706ec4ff29
KimYoonMin <dbsalszz@naver.com>
2026-04-15 21:45:34 +0900

    [#1] Member 데이터 테이블 명시

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS `member` (
                           `member_id` bigint NOT NULL AUTO_INCREMENT,
                           `email` varchar(255) NOT NULL,
                           `password` varchar(80) NOT NULL,
-                          `username` varchar(50) NOT NULL,
                           PRIMARY KEY (`member_id`),
                           UNIQUE KEY `uk_member_email` (`email`)
 );
