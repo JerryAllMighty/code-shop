33e0c056aec7eac7a6c9df940b992ef39106de09
KimYoonMin <dbsalszz@naver.com>
2026-04-18 12:43:48 +0900

    [#2] 회원가입시 회원 중복 확인 기능 추가

--- src/main/java/codeshop/codeshop/application/MemberServiceImpl.java
+++ src/main/java/codeshop/codeshop/application/MemberServiceImpl.java
@@ -3,8 +3,12 @@ package codeshop.codeshop.application;
 import codeshop.codeshop.domain.entity.Member;
 import codeshop.codeshop.infra.MemberRepository;
 import codeshop.codeshop.presentation.dto.SignUpRequestDto;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MemberServiceImpl implements MemberService {
@@ -19,8 +23,17 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public Member signUp(SignUpRequestDto signUpRequestDto) {
         String email = signUpRequestDto.getEmail();
+        validatEmail(email);
+
         String password = signUpRequestDto.getPassword();
         Member member = Member.createForSignUp(email, passwordEncoder.encode(password));
         return memberRepository.save(member);
+    }
+
+    private void validatEmail(String email) {
+        memberRepository.findByEmail(email)
+                .ifPresent(member -> {
+                    throw new DuplicateKeyException("이미 사용 중인 이메일입니다");
+                });
     }
 }
