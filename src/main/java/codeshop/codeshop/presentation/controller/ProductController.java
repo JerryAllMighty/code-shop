5d5ea78200b963eb5ab75430074ba822a09c3390
KimYoonMin <dbsalszz@naver.com>
2026-07-22 16:21:00 +0900

    feat: 상품 조회 기능 추가 (#10) - 상품 목록 리턴 dto 변경에 따른 수정 - 상품 상세 dto 리턴 방식으로 변경

--- src/main/java/codeshop/codeshop/presentation/controller/ProductController.java
+++ src/main/java/codeshop/codeshop/presentation/controller/ProductController.java
@@ -1,15 +1,13 @@
 package codeshop.codeshop.presentation.controller;
 
 import codeshop.codeshop.application.ProductService;
-import codeshop.codeshop.domain.entity.Product;
+import codeshop.codeshop.presentation.dto.ProductResponseDto;
 import codeshop.codeshop.presentation.dto.ProductSearchCondition;
-import codeshop.codeshop.presentation.dto.ProductSearchResponseDto;
-import org.springframework.http.HttpStatus;
+import codeshop.codeshop.presentation.dto.ProductSearchListResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 //TODO : 왜 기본적으로 복수형이럣더라..?
 //TODO : api를 붙이네...? 왜?
@@ -24,17 +22,14 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductSearchResponseDto>> getProducts(@ModelAttribute ProductSearchCondition productSearchCondition) {
+    public ResponseEntity<List<ProductSearchListResponseDto>> getProducts(@ModelAttribute ProductSearchCondition productSearchCondition) {
         //TODO : 실제 응답되는 형태 비교해서 responseEntity 활용법도 한 번 정리하기 (베스트 프랙티스)
         return ResponseEntity.ok(productService.getProducts(productSearchCondition));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Product>> getProduct(@PathVariable Long id) {
-        //TODO : 만약 변수가 long이였으면 어떤 문제가 생겼을지?
-        Optional<Product> product = productService.getProduct(id);
-        //TODO : responseDto로 매핑
+    public ResponseEntity<ProductResponseDto> getProduct(@PathVariable Long id) {
         //TODO : dto 변환 책임은 어디가 적절한지?
-        return ResponseEntity.ok(product);
+        return ResponseEntity.ok(productService.getProduct(id));
     }
 }
