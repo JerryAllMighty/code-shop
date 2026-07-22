9fdd345585b99af974d8c9f428f9c4cbb10193b4
KimYoonMin <dbsalszz@naver.com>
2026-07-22 16:19:57 +0900

    feat: 상품 조회 기능 추가 (#10) - 상품 목록 조회 비즈니스 로직 작성 - 상품 상세 조회 비즈니스 로직 작성

--- src/main/java/codeshop/codeshop/application/ProductService.java
+++ src/main/java/codeshop/codeshop/application/ProductService.java
@@ -1,14 +1,13 @@
 package codeshop.codeshop.application;
 
-import codeshop.codeshop.domain.entity.Product;
+import codeshop.codeshop.presentation.dto.ProductResponseDto;
 import codeshop.codeshop.presentation.dto.ProductSearchCondition;
-import codeshop.codeshop.presentation.dto.ProductSearchResponseDto;
+import codeshop.codeshop.presentation.dto.ProductSearchListResponseDto;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductService {
-    List<ProductSearchResponseDto> getProducts(ProductSearchCondition productSearchCondition);
+    List<ProductSearchListResponseDto> getProducts(ProductSearchCondition productSearchCondition);
 
-    Optional<Product> getProduct(Long id);
+    ProductResponseDto getProduct(Long id);
 }
--- src/main/java/codeshop/codeshop/application/ProductServiceImpl.java
+++ src/main/java/codeshop/codeshop/application/ProductServiceImpl.java
@@ -2,13 +2,13 @@ package codeshop.codeshop.application;
 
 import codeshop.codeshop.domain.entity.Product;
 import codeshop.codeshop.infra.ProductRepository;
+import codeshop.codeshop.presentation.dto.ProductResponseDto;
 import codeshop.codeshop.presentation.dto.ProductSearchCondition;
-import codeshop.codeshop.presentation.dto.ProductSearchResponseDto;
+import codeshop.codeshop.presentation.dto.ProductSearchListResponseDto;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -21,26 +21,23 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public List<ProductSearchResponseDto> getProducts(ProductSearchCondition productSearchCondition) {
+    public List<ProductSearchListResponseDto> getProducts(ProductSearchCondition productSearchCondition) {
         List<Product> productList = productRepository.findProductsBySearchCondition(productSearchCondition);
-        //TODO : dto 변환
-        List<ProductSearchResponseDto> productSearchResponseDtoList = new ArrayList<>();
-//        for (Product product : productList) {
-//            productSearchResponseDtoList.add(ProductSearchResponseDto.builder()
-//                    .name(product.getName())
-//                    .price(product.getPrice())
-//                    .quantity(product.getQuantity())
-//                    .isSaleAvailable(product.isSaleAvailable())
-//                    .build());
-//
-//        }
+        List<ProductSearchListResponseDto> productSearchListResponseDtoList = new ArrayList<>();
+        // TODO : 반복문 말고 람다나 다른 걸 활용하는 건 어떤 이유에서일까?
+        for (Product product : productList) {
+            productSearchListResponseDtoList.add(ProductSearchListResponseDto.from(product));
+        }
         //TODO : dto 변환 책임은 어디가 지는게 좋은가?
-        return productSearchResponseDtoList;
+        return productSearchListResponseDtoList;
     }
 
     @Override
-    public Optional<Product> getProduct(Long id) {
+    public ProductResponseDto getProduct(Long id) {
         //TODO : get과 find 알리아스 뭐가 다른가?
-        return productRepository.findOneById(id);
+        return productRepository.findOneById(id)
+                .map(ProductResponseDto::from)
+                //TODO : exception 처리 전략 정하기
+                .orElseThrow(() -> new RuntimeException("상품 데이터가 조회되지 않습니다"));
     }
 }
