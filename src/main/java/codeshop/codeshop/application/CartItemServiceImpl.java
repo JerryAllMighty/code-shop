9f4676cdce714fc00b82892c1c10aff34fb855f8
KimYoonMin <dbsalszz@naver.com>
2026-07-25 16:58:05 +0900

    feat: 장바구니 상품 기능 개발 (#11)
    - 장바구니 상품 서비스 레이어 수정

--- src/main/java/codeshop/codeshop/application/CartItemServiceImpl.java
+++ src/main/java/codeshop/codeshop/application/CartItemServiceImpl.java
@@ -1,7 +1,23 @@
 package codeshop.codeshop.application;
 
+import codeshop.codeshop.presentation.dto.cart.CartItemAddRequestDto;
+import codeshop.codeshop.presentation.dto.cart.CartItemModifyCartItemQuantityRequestDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CartItemServiceImpl implements CartItemService{
+    @Override
+    public void addCartItem(CartItemAddRequestDto cartItemAddRequestDto, String email) {
+
+    }
+
+    @Override
+    public void modifyCartItemQuantity(Long id, CartItemModifyCartItemQuantityRequestDto cartItemModifyCartItemQuantityRequestDto) {
+
+    }
+
+    @Override
+    public void removeCartItem(Long id) {
+
+    }
 }
