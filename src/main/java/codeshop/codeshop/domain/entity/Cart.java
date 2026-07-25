596a2b879bf56a3a9c52fda0de93c2b52e53239a
KimYoonMin <dbsalszz@naver.com>
2026-07-25 16:54:12 +0900

    feat: 장바구니 상품 기능 개발 (#11)
    - 장바구니 엔티티 신규 개발

---
+++ src/main/java/codeshop/codeshop/domain/entity/Cart.java
@@ -0,0 +1,38 @@
+package codeshop.codeshop.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //TODO : 왜 long은 안되다고 했더라?
+    private Long id;
+
+    //TODO : 연관관계 주인 어떻게 잡는지 다시 정립
+    //TODO : 1대1 굳이 잡아줘야하나?
+    //TODO : 양방향은 어떤 기준일 떄 잡았더라?
+    @OneToOne
+    private Member member;
+
+    @OneToMany(mappedBy = "cart")
+    private List<CartItem> cartItemList = new ArrayList<>();
+
+    protected Cart() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public List<CartItem> getCartItemList() {
+        return cartItemList;
+    }
+}
