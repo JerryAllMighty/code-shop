06291401b14475248642fe8b7d74e6aeb08a605a
KimYoonMin <dbsalszz@naver.com>
2026-08-10 14:22:24 +0900

    refactor: MemberController 엔드포인트 수정 (#14)
    - 백엔드 요청 구분을 위한 /api 구분자 추가
    - 버전 정보 구분을 위한 /v1 추가

--- src/main/java/codeshop/codeshop/presentation/controller/MemberController.java
+++ src/main/java/codeshop/codeshop/presentation/controller/MemberController.java
@@ -1,8 +1,10 @@
 package codeshop.codeshop.presentation.controller;
 
 import codeshop.codeshop.application.MemberService;
-import codeshop.codeshop.presentation.dto.MemberModifyRequestDto;
-import codeshop.codeshop.presentation.dto.SignUpRequestDto;
+import codeshop.codeshop.presentation.dto.request.member.ModifyProfileRequestDto;
+import codeshop.codeshop.presentation.dto.request.member.SignUpRequestDto;
+import codeshop.codeshop.presentation.dto.response.member.ModifyProfileResponseDto;
+import codeshop.codeshop.presentation.dto.response.member.SignUpResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -10,8 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+
 @RestController
-@RequestMapping("/members")
+@RequestMapping("/api/v1/members")
 @Tag(name = "Member", description = "회원 관련 API")
 public class MemberController {
 
@@ -23,17 +27,20 @@ public class MemberController {
 
     @Operation(summary = "회원 가입", description = "새로운 회원을 생성한다")
     @PostMapping("/sign-up")
-    public ResponseEntity<HttpStatus> signUp(@Valid @RequestBody SignUpRequestDto signUpRequestDto) {
-        memberService.signUp(signUpRequestDto);
-        return ResponseEntity.ok(HttpStatus.CREATED);
+    public ResponseEntity<SignUpResponseDto> signUp(@Valid @RequestBody SignUpRequestDto signUpRequestDto) {
+        SignUpResponseDto signUpResponseDto = memberService.signUp(signUpRequestDto);
+        return ResponseEntity.
+                created(URI.create("" + signUpResponseDto.getMemberId()))
+                .body(signUpResponseDto);
     }
 
     @Operation(summary = "회원 정보 수정", description = "회원 정보를 수정한다")
     @PatchMapping("/me")
-    public ResponseEntity<HttpStatus> modifyProfile(@Valid @RequestBody MemberModifyRequestDto memberModifyRequestDto) {
+    public ResponseEntity<ModifyProfileResponseDto> modifyProfile(@RequestBody @Valid ModifyProfileRequestDto modifyProfileRequestDto) {
         //TODO : 이메일 포함해서 보내면 어떻게 되는지? 에러처리할지? 에러가 없다면 그냥 냅둘지?
-        memberService.modifyProfile(memberModifyRequestDto);
-        //TODO : 적절한 상태값 고민해보기
-        return ResponseEntity.ok(HttpStatus.OK);
+        ModifyProfileResponseDto modifyProfileResponseDto = memberService.modifyProfile(modifyProfileRequestDto);
+        return ResponseEntity.
+                status(HttpStatus.OK)
+                .body(modifyProfileResponseDto);
     }
 }
