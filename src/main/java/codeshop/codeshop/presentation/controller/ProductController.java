20dbe389dfbb9e25679cdd37b03eb80fb8d1a18c
KimYoonMin <dbsalszz@naver.com>
2026-08-10 14:22:50 +0900

    refactor: ProductController 엔드포인트 수정 (#14)
    - 백엔드 요청 구분을 위한 /api 구분자 추가
    - 버전 정보 구분을 위한 /v1 추가

--- src/main/java/codeshop/codeshop/presentation/controller/ProductController.java
+++ src/main/java/codeshop/codeshop/presentation/controller/ProductController.java
@@ -1,18 +1,17 @@
 package codeshop.codeshop.presentation.controller;
 
 import codeshop.codeshop.application.ProductService;
-import codeshop.codeshop.presentation.dto.ProductResponseDto;
-import codeshop.codeshop.presentation.dto.ProductSearchCondition;
-import codeshop.codeshop.presentation.dto.ProductSearchListResponseDto;
+import codeshop.codeshop.presentation.dto.response.product.GetProductResponseDto;
+import codeshop.codeshop.presentation.dto.request.product.ProductSearchCondition;
+import codeshop.codeshop.presentation.dto.response.product.GetProductsResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//TODO : 왜 기본적으로 복수형이럣더라..?
 //TODO : api를 붙이네...? 왜?
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
     private final ProductService productService;
@@ -22,13 +21,13 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductSearchListResponseDto>> getProducts(@ModelAttribute ProductSearchCondition productSearchCondition) {
+    public ResponseEntity<List<GetProductsResponseDto>> getProducts(@ModelAttribute ProductSearchCondition productSearchCondition) {
         //TODO : 실제 응답되는 형태 비교해서 responseEntity 활용법도 한 번 정리하기 (베스트 프랙티스)
         return ResponseEntity.ok(productService.getProducts(productSearchCondition));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductResponseDto> getProduct(@PathVariable Long id) {
+    public ResponseEntity<GetProductResponseDto> getProduct(@PathVariable Long id) {
         //TODO : dto 변환 책임은 어디가 적절한지?
         return ResponseEntity.ok(productService.getProduct(id));
     }
