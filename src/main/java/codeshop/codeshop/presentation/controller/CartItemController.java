918af7698e35f11586ed74acafd446d8a1e463a7
KimYoonMin <dbsalszz@naver.com>
2026-08-10 14:23:13 +0900

    refactor: CartItemController 엔드포인트 수정 (#14)
    - 백엔드 요청 구분을 위한 /api 구분자 추가
    - 버전 정보 구분을 위한 /v1 추가

--- src/main/java/codeshop/codeshop/presentation/controller/CartItemController.java
+++ src/main/java/codeshop/codeshop/presentation/controller/CartItemController.java
@@ -1,8 +1,8 @@
 package codeshop.codeshop.presentation.controller;
 
 import codeshop.codeshop.application.CartItemService;
-import codeshop.codeshop.presentation.dto.cart.CartItemAddRequestDto;
-import codeshop.codeshop.presentation.dto.cart.CartItemModifyCartItemQuantityRequestDto;
+import codeshop.codeshop.presentation.dto.request.cart.AddCartItemRequestDto;
+import codeshop.codeshop.presentation.dto.request.cart.ModifyCartItemQuantityRequestDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/carts/items")
+@RequestMapping("/api/v1/carts/items")
 @RestController
 public class CartItemController {
 
@@ -21,24 +21,24 @@ public class CartItemController {
     }
 
     @PostMapping
-    public ResponseEntity<HttpStatus> addCartItem(@Valid CartItemAddRequestDto cartItemAddRequestDto, HttpServletRequest request) {
+    public ResponseEntity<HttpStatus> addCartItem(@Valid AddCartItemRequestDto addCartItemRequestDto, HttpServletRequest request) {
         HttpSession session = request.getSession();
         if (session == null) {
             throw new RuntimeException("로그인이 필요합니다");
         }
         String email = session.getAttribute("email").toString();
-        cartItemService.addCartItem(cartItemAddRequestDto, email);
+        cartItemService.addCartItem(addCartItemRequestDto, email);
         return ResponseEntity.ok(HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<HttpStatus> modifyCartItemQuantity(@PathVariable Long id, @Valid CartItemModifyCartItemQuantityRequestDto cartItemModifyCartItemQuantityRequestDto
+    public ResponseEntity<HttpStatus> modifyCartItemQuantity(@PathVariable Long id, @Valid ModifyCartItemQuantityRequestDto modifyCartItemQuantityRequestDto
     , HttpServletRequest request) {
         HttpSession session = request.getSession();
         if (session == null) {
             throw new RuntimeException("로그인이 필요합니다");
         }
-        cartItemService.modifyCartItemQuantity(id, cartItemModifyCartItemQuantityRequestDto);
+        cartItemService.modifyCartItemQuantity(id, modifyCartItemQuantityRequestDto);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
