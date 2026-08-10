d8ea89fcd300e70db1d0d7c89f19927b54613d9d
KimYoonMin <dbsalszz@naver.com>
2026-08-10 14:23:00 +0900

    refactor: CartController 엔드포인트 수정 (#14)
    - 백엔드 요청 구분을 위한 /api 구분자 추가
    - 버전 정보 구분을 위한 /v1 추가

--- src/main/java/codeshop/codeshop/presentation/controller/CartController.java
+++ src/main/java/codeshop/codeshop/presentation/controller/CartController.java
@@ -1,13 +1,13 @@
 package codeshop.codeshop.presentation.controller;
 
 import codeshop.codeshop.application.CartService;
-import codeshop.codeshop.presentation.dto.cart.MyCartResponseDto;
+import codeshop.codeshop.presentation.dto.response.cart.GetMyCartResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/carts")
+@RequestMapping("/api/v1/carts")
 @RestController
 public class CartController {
 
@@ -18,7 +18,7 @@ public class CartController {
     }
 
     @GetMapping
-    public ResponseEntity<MyCartResponseDto> getMyCart(HttpServletRequest request) {
+    public ResponseEntity<GetMyCartResponseDto> getMyCart(HttpServletRequest request) {
         HttpSession session = request.getSession();
         if (session == null) {
             throw new RuntimeException("로그인이 필요합니다");
