3b2dcee61540c07131db8306a08b5a1de3683864
KimYoonMin <dbsalszz@naver.com>
2026-08-18 04:11:25 +0900

    refactor: GetProductResponseDto 수정 (#16)
    - record 로 변경

--- src/main/java/codeshop/codeshop/presentation/dto/response/product/GetProductResponseDto.java
+++ src/main/java/codeshop/codeshop/presentation/dto/response/product/GetProductResponseDto.java
@@ -1,27 +1,11 @@
 package codeshop.codeshop.presentation.dto.response.product;
 
 import codeshop.codeshop.domain.entity.Product;
-import lombok.Getter;
 
-@Getter
-public class GetProductResponseDto {
-    private final String name;
-    private final long price;
-    private final long quantity;
-    private final boolean isPurchasable;
-
-    public GetProductResponseDto(String name, long price, long quantity, boolean isPurchasable) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-        this.isPurchasable = isPurchasable;
-    }
-
-    public static GetProductResponseDto from(Product product) {
-        String name = product.getName();
-        long price = product.getPrice();
-        long quantity = product.getQuantity();
-        boolean isPurchasable = Product.isPurchasable(product);
-        return new GetProductResponseDto(name, price, quantity, isPurchasable);
-    }
+public record GetProductResponseDto(
+        String name,
+        Long price,
+        Long quantity,
+        Boolean isPurchasable
+) {
 }
