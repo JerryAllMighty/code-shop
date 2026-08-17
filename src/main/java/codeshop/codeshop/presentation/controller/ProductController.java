3a0aec4797174f4553347510db686836189b3f47
KimYoonMin <dbsalszz@naver.com>
2026-08-18 04:23:19 +0900

    design: ProductController  (#12)
    - 상품 목록 조회 네이밍 변경 / 유스케이스와 더 가깝게 변경

--- src/main/java/codeshop/codeshop/presentation/controller/ProductController.java
+++ src/main/java/codeshop/codeshop/presentation/controller/ProductController.java
@@ -2,14 +2,13 @@ package codeshop.codeshop.presentation.controller;
 
 import codeshop.codeshop.application.ProductService;
 import codeshop.codeshop.presentation.dto.response.product.GetProductResponseDto;
-import codeshop.codeshop.presentation.dto.request.product.ProductSearchCondition;
-import codeshop.codeshop.presentation.dto.response.product.GetProductsResponseDto;
+import codeshop.codeshop.presentation.dto.request.product.SearchProductCondition;
+import codeshop.codeshop.presentation.dto.response.product.SearchProductResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//TODO : api를 붙이네...? 왜?
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
@@ -21,14 +20,12 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<List<GetProductsResponseDto>> getProducts(@ModelAttribute ProductSearchCondition productSearchCondition) {
-        //TODO : 실제 응답되는 형태 비교해서 responseEntity 활용법도 한 번 정리하기 (베스트 프랙티스)
-        return ResponseEntity.ok(productService.getProducts(productSearchCondition));
+    public ResponseEntity<List<SearchProductResponseDto>> searchProduct(@ModelAttribute SearchProductCondition searchProductCondition) {
+        return ResponseEntity.ok(productService.getProducts(searchProductCondition));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<GetProductResponseDto> getProduct(@PathVariable Long id) {
-        //TODO : dto 변환 책임은 어디가 적절한지?
         return ResponseEntity.ok(productService.getProduct(id));
     }
 }
