3c00d0f058f9bef4ec8d532408cce10d4cb7d5bd
KimYoonMin <dbsalszz@naver.com>
2026-08-18 04:12:10 +0900

    refactor: ModifyProfileRequestDto 수정 (#16)
    - record 로 변경

--- src/main/java/codeshop/codeshop/presentation/dto/request/member/ModifyProfileRequestDto.java
+++ src/main/java/codeshop/codeshop/presentation/dto/request/member/ModifyProfileRequestDto.java
@@ -3,16 +3,15 @@ package codeshop.codeshop.presentation.dto.request.member;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
 
-@Getter
-public class ModifyProfileRequestDto {
-    @NotNull(message = "이메일은 필수 입력값입니다")
-    @Email(message = "잘못된 이메일 형식입니다")
-    @Size(max = 255, message = "이메일 길이는 255자 이하여야 합니다")
-    private String email;
+public record ModifyProfileRequestDto(
+        @NotNull(message = "이메일은 필수 입력값입니다")
+        @Email(message = "잘못된 이메일 형식입니다")
+        @Size(max = 255, message = "이메일 길이는 255자 이하여야 합니다")
+        String email,
 
-    @NotNull(message = "비밀번호는 필수 입력값입니다")
-    @Size(min = 8, max = 20, message = "비밀번호는 8자 ~ 20자까지 허용됩니다")
-    private String password;
+        @NotNull(message = "비밀번호는 필수 입력값입니다")
+        @Size(min = 8, max = 20, message = "비밀번호는 8자 ~ 20자까지 허용됩니다")
+        String password
+) {
 }
