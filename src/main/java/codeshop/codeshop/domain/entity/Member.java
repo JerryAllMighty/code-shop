45be3a3e08018ff1d5feef34502539463ed5bb92
KimYoonMin <dbsalszz@naver.com>
2026-08-18 15:43:02 +0900

    feat: Member 엔티티 수정  (#1)
    - 접근 제어 레벨 protected로 변경

--- src/main/java/codeshop/codeshop/domain/entity/Member.java
+++ src/main/java/codeshop/codeshop/domain/entity/Member.java
@@ -1,13 +1,14 @@
 package codeshop.codeshop.domain.entity;
 
-import codeshop.codeshop.presentation.dto.member.MemberModifyRequestDto;
+import codeshop.codeshop.presentation.dto.request.member.ModifyProfileRequestDto;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
-
-import java.util.Objects;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,9 +17,6 @@ public class Member {
     private String email;
 
     private String password;
-
-    protected Member() {
-    }
 
     private Member(String password) {
         this.password = password;
@@ -33,22 +31,10 @@ public class Member {
         return new Member(email, encryptedPassword);
     }
 
-    public static Member createForUpdateProfile(MemberModifyRequestDto memberModifyRequestDto) {
+    public static Member createForUpdateProfile(ModifyProfileRequestDto modifyProfileRequestDto) {
         //TODO : 만약 회원정보 수정에 필요한 정보가 늘어난다면..? 그 떄는 엔티티와 여길 참조하는 모든 곳을 바꿔줘야하나? 더 좋은 방법은 없나?
         //TODO : dto를 레포지토리에 넘기는 방법은 어떤지?
-        String encryptedPassword = memberModifyRequestDto.getPassword();
+        String encryptedPassword = modifyProfileRequestDto.password();
         return new Member(encryptedPassword);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Member member = (Member) o;
-        return Objects.equals(id, member.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
     }
 }
