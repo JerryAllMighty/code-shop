b84097c7338cbf5a3653429e1f58c062a3bd83e8
KimYoonMin <dbsalszz@naver.com>
2026-08-18 13:54:20 +0900

    design: Product 수정 (#10)
    - JPA 엔티티 생성시 프록시 생성 메커니즘과 맞추기 위해 기본 생성자 protected로 수정

--- src/main/java/codeshop/codeshop/domain/entity/Product.java
+++ src/main/java/codeshop/codeshop/domain/entity/Product.java
@@ -4,32 +4,27 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //TODO : product랑 붙여서 네이밍해야하는지?
     private String name;
 
-    //TODO : long이 적절한지?
     private long price;
 
     private long quantity;
 
-    //TODO : Boolean이랑 또 뭐가 다를지?
     private boolean isOnSale;
 
-    public static boolean isPurchasable(Product product) {
-        long quantity = product.getQuantity();
-        boolean isOnSale = product.isOnSale();
+    private boolean isPurchasable() {
         return quantity > 0 && isOnSale;
-    }
-
-    protected Product() {
     }
 }
